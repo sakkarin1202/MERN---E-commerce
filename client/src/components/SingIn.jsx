49765f76199a84009e2084def8018b8726012eb1
sakkarin1202 <xfake12340@hotmail.com>
2025-03-08 21:08:52 +0700

    update

--- client/src/components/SingIn.jsx
+++ client/src/components/SingIn.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { FaGoogle, FaFacebook, FaGithub } from "react-icons/fa";
 import { useForm } from "react-hook-form";
-import { AuthContext } from "../context/Authcontext";
+import { AuthContext } from "../context/AuthContext";
 import Swal from "sweetalert2";
 import { useNavigate, useLocation } from "react-router";
 
